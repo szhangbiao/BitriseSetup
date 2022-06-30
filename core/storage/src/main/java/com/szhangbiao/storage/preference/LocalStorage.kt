e6474878c16006295436c8df31ae82c23d46d82b
Barry <szhangbiao@gmail.com>
2022-06-30 22:47:14 +0800

    create base structure

---
+++ core/storage/src/main/java/com/szhangbiao/storage/preference/LocalStorage.kt
@@ -0,0 +1,4 @@
+package com.szhangbiao.storage.preference
+
+interface LocalStorage {
+}
---
+++ core/storage/src/main/java/com/szhangbiao/storage/preference/LocalStorageImpl.kt
@@ -0,0 +1,7 @@
+package com.szhangbiao.storage.preference
+
+import android.content.Context
+
+class LocalStorageImpl(context: Context) : LocalStorage {
+
+}
